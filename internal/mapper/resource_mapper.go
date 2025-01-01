058a9449252b1e60a8daea72aaad7e654d187deb
Geun-Oh <kandy1002@naver.com>
2025-01-02 00:02:44 +0900

    feat: add request parameters that are used only in update operation in schema

--- internal/mapper/resource_mapper.go
+++ internal/mapper/resource_mapper.go
@@ -214,8 +214,25 @@ func generateResourceSchema(logger *slog.Logger, explorerResource explorer.Resou
 		readParameterAttributes = append(readParameterAttributes, parameterAttribute)
 	}
 
+	// ********************
+	// Update Request Body (optional)
+	// ********************
+	logger.Debug("searching for update operation request body")
+
+	schemaOpts = oas.SchemaOpts{
+		Ignores: explorerResource.SchemaOptions.Ignores,
+	}
+	updateRequestSchema, err := oas.BuildSchemaFromRequest(explorerResource.UpdateOps[0], schemaOpts, oas.GlobalSchemaOpts{})
+	if err != nil {
+		return nil, err
+	}
+	updateRequestAttributes, schemaErr := updateRequestSchema.BuildResourceAttributes()
+	if schemaErr != nil {
+		return nil, schemaErr
+	}
+
 	// TODO: currently, no errors can be returned from merging, but in the future we should consider raising errors/warnings for unexpected scenarios, like type mismatches between attribute schemas
-	resourceAttributes, _ := createRequestAttributes.Merge(createResponseAttributes, readResponseAttributes, readParameterAttributes)
+	resourceAttributes, _ := createRequestAttributes.Merge(createResponseAttributes, readResponseAttributes, readParameterAttributes, updateRequestAttributes)
 
 	// TODO: handle error for overrides
 	resourceAttributes, _ = resourceAttributes.ApplyOverrides(explorerResource.SchemaOptions.AttributeOptions.Overrides)
