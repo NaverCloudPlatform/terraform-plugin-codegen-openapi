0963c44886418d110de871ea440e7e5464ce67eb
Geun-Oh <kandy1002@naver.com>
2025-01-10 16:36:57 +0900

    fix: use ClearDoubleQuote from client.go

--- internal/sdk/template.go
+++ internal/sdk/template.go
@@ -175,7 +175,7 @@ func getPath(path string) string {
 		if start == -1 {
 			s = s + fmt.Sprintf(`"%s"`, val)
 		} else {
-			s = s + fmt.Sprintf(`clearDoubleQuote(r.%s)`, PathToPascal(val))
+			s = s + fmt.Sprintf(`ClearDoubleQuote(r.%s)`, PathToPascal(val))
 		}
 	}
 
